9788dfe0269ebd8fcea7cb9bd3f3d1ba34d0fcdb
Sam Alghanmi <oalghnmi@gmail.com>
2018-05-22 10:46:12 -0400

    todo app

---
+++ src/javascript/client.js
@@ -0,0 +1,16 @@
+import TodoStore from './stores/TodoStore';
+import ViewStore from './stores/ViewStore';
+import TodoApp from './components/todoApp.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const initialState = window.initialState && JSON.parse(window.initialState) || {};
+
+var todoStore = TodoStore.fromJS(initialState.todos || []);
+var viewStore = new ViewStore();
+
+ReactDOM.render(
+	<TodoApp todoStore={todoStore} viewStore={viewStore}/>,
+	document.getElementById('todoapp')
+);
+
