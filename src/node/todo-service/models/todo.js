8ca615a6e4e51cce517427b637db85f0c806b6d2
salghanmi <salghanmi@touchtown.us>
2018-07-07 20:25:47 -0400

    todo service

--- src/node/todo-service/models/todo.js
+++ src/node/todo-service/models/todo.js
@@ -1,14 +1,22 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 // set up a mongoose model
-module.exports = mongoose.model('Todo', new Schema({
-	todo_id: String,
-	title: String,
-	points: Number,
-	date: Date,
-	category: String,
-	completed: Boolean,
-	tags: [String],
-	deleted: Boolean
-}, { timestamps: true }));
+const Todo = mongoose.model(
+  "Todo",
+  new Schema(
+    {
+      todo_id: String,
+      title: String,
+      points: Number,
+      date: Date,
+      category: String,
+      completed: Boolean,
+      tags: [String],
+      deleted: Boolean
+    },
+    { timestamps: true }
+  )
+);
+
+module.exports = Todo;
--- src/node/todo-service/todo-service.js
+++ src/node/todo-service/todo-service.js
@@ -1,49 +1,69 @@
 // basic route (http://localhost:8080)
-const express = require('express');
-var busboy = require('connect-busboy');
+const express = require("express");
+var busboy = require("connect-busboy");
 
 var apiRoutes = express.Router();
 
-export default function ({
-  app,
-  Todo
-}) {
-
+export default function({ app, Todo }) {
   //busboy is for uploading multipart forms (csv files here)
   app.use(busboy());
 
-  apiRoutes.get('/', function (req, res) {
-    res.send('Hello! this is budgetqt backend!');
+  apiRoutes.get("/", function(req, res) {
+    res.send("Hello! this is budgetqt backend!");
   });
 
-  apiRoutes.get('/todos', function (req, res) {
-    Todo.find({}).sort('-date').exec((err, data) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send(err);
-      }
-      res.send(data);
+  apiRoutes.get("/todos", function(req, res) {
+    Todo.find({})
+      .sort("-date")
+      .exec((err, data) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send(err);
+        }
+        res.send(data);
+      });
+  });
+  const findOneOrCreate = (condition, doc, callback) => {
+    Todo.findOne(condition, (err, result) => {
+      return result
+        ? callback(err, result)
+        : Todo.create(doc, (err, result) => {
+            return callback(err, result);
+          });
     });
-  });
-
-  const findOrUpdateTodo = (todo) => {
+  };
+  const findOrUpdateTodo = todo => {
+    let newTodo = {
+      title: todo.title,
+      completed: todo.completed,
+      todo_id: todo.todo_id,
+      deleted: todo.deleted
+    };
     return new Promise((resolve, reject) => {
-      Todo.findOneAndUpdate({ title: todo.title }, todo, {
-        upsert: true
-      }, function (err, doc) {
+      Todo.findOneAndUpdate(
+        { title: todo.title },
+        newTodo,
+        {
+          upsert: true
+        },
+        function(err, doc) {
+          if (err) return reject(err);
+          return resolve(todo);
+        }
+      );
+    });
+  };
+  const findTodo = todo => {
+    return new Promise((resolve, reject) => {
+      Todo.findOne({ title: todo.title, todo_id: title.todo_id }, function(
+        err,
+        todo
+      ) {
         if (err) return reject(err);
         return resolve(todo);
       });
-    })
-  }
-  const findTodo = (todo) => {
-    return new Promise((resolve, reject) => {
-      Todo.findOne({ title: todo.title, todo_id: title.todo_id }, function (err, todo) {
-        if (err) return reject(err);
-        return resolve(todo);
-      });
-    })
-  }
+    });
+  };
   const allTodos = () => {
     return new Promise((resolve, reject) => {
       Todo.find({}, (err, todos) => {
@@ -51,75 +71,99 @@ export default function ({
           return reject(err);
         }
         return resolve(todos);
-      })
-    })
-
-  }
-  apiRoutes.post('/todos', function (req, res) {
+      });
+    });
+  };
+  apiRoutes.post("/todos", function(req, res) {
     let todos = req.body.todos;
     let updateTodoPromises = todos.map(todo => findOrUpdateTodo(todo));
-    allTodos().then(allTodos => {
-      let leftOver = allTodos.filter(e => {
-        let found = todos.filter(todo => {
-          return todo.todo_id === e.todo_id
-        })[0];
-        return !found;
+    Promise.all(updateTodoPromises).then(values => {
+      allTodos().then(allTodos => {
+        let leftOver = allTodos.filter(e => {
+          let found = todos.filter(todo => {
+            return todo.todo_id === e.todo_id;
+          })[0];
+          return !found;
+        });
+        leftOver.map(todo => {
+          todo.deleted = true;
+          findOrUpdateTodo(todo)
+            .then(t => {})
+            .catch(e => {
+              console.log(e);
+            });
+        });
       });
-      leftOver.map(todo=>{
-        todo.deleted = true;
-        findOrUpdateTodo(todo);
+    });
+    Promise.all(updateTodoPromises)
+      .then(values => {
+        res.send(values);
       })
-    })
-    Promise.all(updateTodoPromises).then((values) => {
-      res.send(values)
-    }).catch(err => {
-      res.status(500).send(err);
-    })
+      .catch(err => {
+        res.status(500).send(err);
+      });
   });
 
-  apiRoutes.put('/todos', (req, res) => {
+  apiRoutes.put("/todos", (req, res) => {
     //take the imported todo, format it and add it to the todos collection
     let todo = req.body;
-    let newTodo = { title: todo.title, amount: todo.amount, date: todo.date, tags: todo.tags }
-    Todo.findOneAndUpdate({ _id: todo._id }, newTodo, {
-      upsert: false
-    }, function (err, doc) {
-      if (err) return res.send(500, { error: err });
-      res.send(newTodo);
-    });
+    let newTodo = {
+      title: todo.title,
+      amount: todo.amount,
+      date: todo.date,
+      tags: todo.tags
+    };
+    Todo.findOneAndUpdate(
+      { _id: todo._id },
+      newTodo,
+      {
+        upsert: false
+      },
+      function(err, doc) {
+        if (err) return res.send(500, { error: err });
+        res.send(newTodo);
+      }
+    );
   });
 
-  apiRoutes.delete('/todos', (req, res) => {
+  apiRoutes.delete("/todos", (req, res) => {
     let todo = req.body;
     //remove the imported todo
     Todo.find({
       _id: todo["_id"]
-    }).remove().exec((err) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      res.status(200).send();
-    });
+    })
+      .remove()
+      .exec(err => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.status(200).send();
+      });
   });
 
-  apiRoutes.get('/todos/export/csv', (req, res) => {
+  apiRoutes.get("/todos/export/csv", (req, res) => {
     Todo.find({}, (err, data) => {
       if (err) {
         console.log(err);
         return res.status(500).send(err);
       }
-      res.setHeader('Content-disposition', 'attachment; filename=todos.csv');
-      res.set('Content-Type', 'text/csv');
+      res.setHeader("Content-disposition", "attachment; filename=todos.csv");
+      res.set("Content-Type", "text/csv");
       let csvFile = csvConverter(data);
       res.status(200).send(csvFile);
     });
   });
 
-  apiRoutes.post('/todos/upload/csv', function (req, res) {
+  apiRoutes.post("/todos/upload/csv", function(req, res) {
     if (req.busboy) {
-      req.busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-        file.pipe(csv()).on('data', (entry) => {
-
+      req.busboy.on("file", function(
+        fieldname,
+        file,
+        filename,
+        encoding,
+        mimetype
+      ) {
+        file.pipe(csv()).on("data", entry => {
           var todo = parser({
             entry
           });
@@ -128,22 +172,31 @@ export default function ({
 
           if (!hasPaymentTags(todo.tags)) {
             return Todo.update(
-              { date: todo.date, amount: todo.amount, tags: { $in: todo.tags } },
+              {
+                date: todo.date,
+                amount: todo.amount,
+                tags: { $in: todo.tags }
+              },
               { $setOnInsert: todo },
               { upsert: true },
-              function (err, numAffected) {
+              function(err, numAffected) {
                 if (err) {
                   return console.log("ERROR", err);
                 }
               }
             );
           }
-        })
+        });
       });
-      req.busboy.on('field', function (key, value, keyTruncated, valueTruncated) { });
+      req.busboy.on("field", function(
+        key,
+        value,
+        keyTruncated,
+        valueTruncated
+      ) {});
       req.pipe(req.busboy);
     }
-    res.send('You have uploaded the file!');
+    res.send("You have uploaded the file!");
   });
 
   return apiRoutes;
